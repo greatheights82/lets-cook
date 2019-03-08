73696783b82850c831348712d7926f4b855edcfe
Jake Hebert <greatheights@gmail.com>
2019-03-08 11:57:07 -0600

    initial redux set up

---
+++ client/components/Main.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Footer, FooterTab, Button, Text } from 'native-base'
+
+export default class FooterMenu extends React.Component {
+  render() {
+    return (
+      <Content>
+        <Text style={styles.title}>Let's Cook!</Text>
+      </Content>
+    )
+  }
+}
---
+++ client/redux/recipes.js
@@ -0,0 +1,12 @@
+import axios from 'axios'
+
+// ACTION TYPES
+
+// ACTION CREATORS
+
+// THUNKS
+
+// REDUCER
+
+export const recipes = (state = {}, action) => {}
+export default recipes
