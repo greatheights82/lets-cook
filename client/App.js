673d22b93c27297cc1903569acd2822771a51d99
Jake Hebert <greatheights@gmail.com>
2019-03-08 11:14:36 -0600

    front-end set up

--- client/App.js
+++ client/App.js
@@ -1,18 +1,35 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text } from 'react-native'
+import {
+  Container,
+  Content,
+  Footer,
+  Header,
+  Button,
+  Icon,
+  Left,
+  Body,
+  Title,
+  Right,
+} from 'native-base'
+import { getStatusBarHeight } from 'react-native-status-bar-height'
 import FooterMenu from './components/FooterMenu'
 import { AppLoading, Font } from 'expo'
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    // flex: 1,
     backgroundColor: 'green',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
   title: {
     fontSize: 20,
     fontWeight: 'bold',
+  },
+  header: {
+    paddingTop: getStatusBarHeight(),
+    height: 54 + getStatusBarHeight(),
   },
 })
 
@@ -34,10 +51,23 @@ export default class App extends React.Component {
       )
     } else {
       return (
-        <View style={styles.container}>
-          <Text style={styles.title}>Let's Cook!</Text>
+        <Container style={styles.container}>
+          <Header style={styles.header}>
+            <Left>
+              <Button transparent>
+                <Icon name="menu" />
+              </Button>
+            </Left>
+            <Body>
+              <Title>Header</Title>
+            </Body>
+            <Right />
+          </Header>
+          <Content>
+            <Text style={styles.title}>Let's Cook!</Text>
+          </Content>
           <FooterMenu />
-        </View>
+        </Container>
       )
     }
   }
